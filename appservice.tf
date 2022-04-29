831cd77aca1875423555aba1b82ae063f19e0082
Navnit-S <60913017+Navnit-S@users.noreply.github.com>
2022-04-29 12:21:18 +0530

    Add files via upload

---
+++ appservice.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = "West Europe"
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_windows_web_app" "example" {
+  name                = "elehant"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
